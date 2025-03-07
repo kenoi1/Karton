47527c19bfb33e476a6a8c605a9c13acd4e4540d
Derek Lin <derekhongdalin@gmail.com>
2025-03-07 17:34:09 -0500

    license

--- src/vmlistmodel.cpp
+++ src/vmlistmodel.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 Derek Lin <derekhongdalin@gmail.com>
+
 #include "vmlistmodel.h"
 
 VM::VM (const QString& domainName, const QString& uuid, const bool& isRunning) {
--- src/vmlistmodel.h
+++ src/vmlistmodel.h
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 Derek Lin <derekhongdalin@gmail.com>
+
 #pragma once
 
 #include <QObject>
