ef728473ec10d7e9e0d85d6643c4ee582aec190e
Derek Lin <derekhongdalin@gmail.com>
2025-03-16 01:28:31 -0400

    fix update bug

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,10 @@ int main(int argc, char *argv[])
 
     qDebug() << "Hello! Starting application...";
     Karton karton;
-
     VMModel *model = new VMModel(&karton);
-    model->updateDomains();
+    engine.rootContext()->setContextProperty(QStringLiteral("Karton"), &karton);
     engine.rootContext()->setContextProperty(QStringLiteral("VMModel"), model);
 
-    engine.rootContext()->setContextProperty(QStringLiteral("Karton"), &karton);
 
     engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
     engine.loadFromModule("org.kde.karton", "Main");
