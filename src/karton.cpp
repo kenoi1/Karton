f888feb85a2b2b48109ed740b878c01f8905f4a4
Derek Lin <derekhongdalin@gmail.com>
2025-03-28 02:41:16 -0400

    domain installer modules

--- src/karton.cpp
+++ src/karton.cpp
@@ -280,47 +280,8 @@ bool Karton::viewDomain(const Domain *domain)
 
 bool Karton::createDomain(const QString &name, const QString &osVariant, const float memoryGB, const float storageGB, const QString &diskPath, const int cpus)
 {
-    QFile xmlDomain(QStringLiteral("xmlSample.xml"));
-    if (!xmlDomain.open(QFile::WriteOnly | QFile::Text)) {
-        qCritical() << "xmlDomain opened in another instance or something??";
-        return false;
-    }
-    
-    QDomDocument document;
-    QDomElement root = document.createElement(QStringLiteral("domain"));
-    root.setAttribute(QStringLiteral("type"), QStringLiteral("kvm"));
-    
-    virDomainPtr *domains = nullptr;
-    int numDomains = virConnectListAllDomains(m_conn, &domains, 0);
-    root.setAttribute(QStringLiteral("id"), QString::number(numDomains + 1));
-    document.appendChild(root);
-    
-    QDomElement nameElement = document.createElement(QStringLiteral("name"));
-    QDomText nameString = document.createTextNode(name);
-    nameElement.appendChild(nameString);
-    root.appendChild(nameElement);
-    
-    QDomElement uuidElement = document.createElement(QStringLiteral("uuid"));
-    QString uuidString = QUuid::createUuid().toString();
-    uuidString.remove(QLatin1Char('{'));
-    uuidString.remove(QLatin1Char('}'));
-    //alternatively
-    // virUUIDGenerate(uuid);
-    // char uuidStr[VIR_UUID_STRING_BUFLEN];
-    // virUUIDFormat(uuid, uuidStr);
-
-    QDomText uuidText = document.createTextNode(uuidString);
-    uuidElement.appendChild(uuidText);
-    root.appendChild(uuidElement);
-    
-    QString xmlString = document.toString(4);
-    QTextStream xmlContent(&xmlDomain);
-    xmlContent << xmlString;
-    
-    qDebug().noquote() << "Generated XML:";
-    qDebug().noquote() << xmlString;
-    
-    xmlDomain.close();
+    DomainInstaller installer;
+    installer.configureXML(m_conn, name, osVariant, memoryGB, storageGB, diskPath, cpus);
     return true;
 }
 
