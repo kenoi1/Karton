3c819e6098ebf786046e95a59b17f07b545d1ac8
Derek Lin <derekhongdalin@gmail.com>
2025-04-01 21:04:54 -0400

    [wip] refactor, configuration class

--- src/domain.h
+++ src/domain.h
@@ -3,9 +3,10 @@
 
 #pragma once
 
-#include <QObject>
 #include <QString>
 #include <libvirt/libvirt.h>
+
+class DomainConfig;
 
 class Domain : public QObject
 {
@@ -28,78 +29,25 @@ Q_SIGNALS:
     void autostartChanged(const bool autostart);
 
 public:
-    Domain(QObject *parent = nullptr);
-    ~Domain() override;
+    explicit Domain(QObject *parent = nullptr);
     Domain(virDomainPtr domainPtr,
-           const QString &name,
-           const QString &uuid,
-           bool isActive,
-           QString state,
-           int maxRam,
-           int ramUsage,
-           int cpus,
-           const QString &diskPath,
-           bool autostart,
+           DomainConfig *config,
            QObject *parent = nullptr);
-    Q_DISABLE_COPY_MOVE(Domain);
+    ~Domain();
 
     // getters
     [[nodiscard]] virDomainPtr domainPtr() const
     {
         return m_domainPtr;
     }
-    [[nodiscard]] QString name() const
-    {
-        return m_name;
-    }
-    [[nodiscard]] QString uuid() const
-    {
-        return m_uuid;
-    }
-    [[nodiscard]] bool isActive() const
-    {
-        return m_isActive;
-    }
-    [[nodiscard]] QString state() const
-    {
-        return m_state;
-    }
-    [[nodiscard]] int maxRam() const
-    {
-        return m_maxRam;
-    }
-    [[nodiscard]] int ramUsage() const
-    {
-        return m_ramUsage;
-    }
-    [[nodiscard]] int cpus() const
-    {
-        return m_cpus;
-    }
-    [[nodiscard]] QString diskPath() const
-    {
-        return m_diskPath;
-    }
-    [[nodiscard]] bool autostart() const
-    {
-        return m_autostart;
-    }
 
-    void setActive(bool active);
-    void setState(const QString &state);
-    void setRamUsage(int ramUsage);
-    void setAutostart(bool autostart);
-    [[nodiscard]] static QString uuidString(virDomainPtr domainPtr);
+    [[nodiscard]] DomainConfig *config() const
+    {
+        return m_config;
+    }
+    
 
 private:
     virDomainPtr m_domainPtr;
-    QString m_name;
-    QString m_uuid;
-    bool m_isActive;
-    QString m_state;
-    int m_maxRam;
-    int m_ramUsage;
-    int m_cpus;
-    QString m_diskPath;
-    bool m_autostart;
+    DomainConfig *m_config;
 };
