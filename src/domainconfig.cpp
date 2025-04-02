fbfe90493ea46b88f8d4c20f583d7f47b993eeb4
Derek Lin <derekhongdalin@gmail.com>
2025-04-01 21:04:54 -0400

    [wip] refactor, configuration class

---
+++ src/domainconfig.cpp
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 Derek Lin <derekhongdalin@gmail.com>
+
+#include "domainconfig.h"
+#include <QString>
+
+DomainConfig::DomainConfig(QObject *parent)
+    : QObject(parent)
+    , m_name(QStringLiteral(""))
+    , m_uuid(QStringLiteral(""))
+    , m_isActive(false)
+    , m_maxRam(0)
+    , m_ramUsage(0)
+    , m_cpus(0)
+    , m_diskPath(QStringLiteral(""))
+    , m_autostart(false)
+{
+}
+
+void DomainConfig::setActive(bool active) {
+    if (m_isActive != active) {
+        m_isActive = active;
+        Q_EMIT isActiveChanged(active);
+    }
+}
+
+void DomainConfig::setState(const QString &state) {
+    if (m_state != state) {
+        m_state = state;
+        Q_EMIT stateChanged(state);
+    }
+}
+
+void DomainConfig::setRamUsage(int ramUsage) {
+    if (m_ramUsage != ramUsage) {
+        m_ramUsage = ramUsage;
+        Q_EMIT ramUsageChanged(ramUsage);
+    }
+}
+
+void DomainConfig::setAutostart(bool autostart) {
+    if (m_autostart != autostart) {
+        m_autostart = autostart;
+        Q_EMIT autostartChanged(autostart);
+    }
+}
