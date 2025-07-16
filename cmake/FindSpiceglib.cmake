2d9cb840de4470f071b598a6d6d56cc09b776a34
Derek Lin <derekhongdalin@gmail.com>
2025-07-15 23:23:18 -0400

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Harald Sitter <sitter@kde.org>

--- cmake/FindSpiceglib.cmake
+++ cmake/FindSpiceglib.cmake
@@ -5,7 +5,7 @@ find_package(PkgConfig REQUIRED)
 pkg_check_modules(SPICEGLIB REQUIRED spice-client-glib-2.0 IMPORTED_TARGET)
 
 if(TARGET PkgConfig::SPICEGLIB AND NOT TARGET Spiceglib::Spiceglib)
-  add_library(Spiceglib::Spiceglib ALIAS PkgConfig::SPICEGLIB)
+    add_library(Spiceglib::Spiceglib ALIAS PkgConfig::SPICEGLIB)
 endif()
 
 mark_as_advanced(SPICEGLIB_INCLUDE_DIR SPICEGLIB_LIBRARY)
